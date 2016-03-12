026f5aeafb4eb2ae45bb5ca5e0e6ae61aeb4ff2a
aleshinsergey <aleshinsergey@outlook.com>
2016-03-12 12:04:56 +0300

    Remove incorrect path to cmake

--- buildMake.sh
+++ buildMake.sh
@@ -9,7 +9,6 @@ export BUILD_DIR=${ROOT_DIR}/build
 export INSTALL_DIR=${ROOT_DIR}/install
 export OPENCV_ROOT_DIR=${ROOT_DIR}/OpenCV/share/OpenCV
 # ===============================================
-export PATH=/home/temik/University/Studier/products/cmake-3.3.0/bin:${PATH}
 
 # ===============================================
 # ==  Prepare project files and build them  =====
