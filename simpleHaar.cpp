3f809ed0c1a002c3cc053cccaaff2ea8f92f85b5
aleshinsergey <aleshin8sergey@gmail.com>
2016-09-21 15:09:09 +0300

    Добавили примитивы Хаара

---
+++ simpleHaar.cpp
@@ -0,0 +1,31 @@
+#include "iostream"
+#include "opencv/cv.hpp"
+#include "opencv/highgui.h"
+
+using namespace cv;
+using namespace std;
+
+int Haar0(int x, int y, int w, int h) {
+	int f;
+	return f;
+}
+int Haar1(int x, int y, int w, int h) {
+	int f;
+	return f;
+}
+int Haar2(int x, int y, int w, int h) {
+	int f;
+	return f;
+}
+int Haar3(int x, int y, int w, int h) {
+	int f;
+	return f;
+}
+int Haar4(int x, int y, int w, int h) {
+	int f;
+	return f;
+}
+int Haar5(int x, int y, int w, int h) {
+	int f;
+	return f;
+}
---
+++ simpleHaar.h
@@ -0,0 +1,7 @@
+#pragma once
+int Haar0(int, int, int, int);
+int Haar1(int, int, int, int);
+int Haar2(int, int, int, int);
+int Haar3(int, int, int, int);
+int Haar4(int, int, int, int);
+int Haar5(int, int, int, int);
